d027ccf8658053fefb8088f40a8a3deafe33f2e2
Gerard Mistretta <gerardmistretta@yahoo.com>
2020-07-28 18:03:18 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -19,11 +19,11 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
-modulo =  (num1 % num2)
+modulo = (num1 % num2)
 modulo
 end
 
 def square_root(num)
-square_root = (num**)
+square_root = (num)
 square_root
 end
